b5bee7daa6184c9e48a7c080bd2b2273da0c71bd
tao_Linux <Tao101365@outlook.com>
2024-04-11 15:41:12 +0800

    HUITiao2

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -23,7 +23,7 @@ public class HelloController {
 	public ModelAndView hello(@PathVariable("name") String name) {
 
 		ModelAndView model = new ModelAndView();
-		model.setViewName("hello");
+		model.setViewName("hello666");
 		model.addObject("msg", name);
 
 		return model;
